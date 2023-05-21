74bf2803142ecad877c15d59db5c27e740e5a6fb
danielcrubens <dcr.tecnologi@gmail.com>
2023-05-20 22:49:42 -0300

    front-page e footer :memo:

--- footer.php
+++ footer.php
@@ -1,7 +1,24 @@
 <?php wp_footer(); ?>
 
-<footer>
-<h1>Footer</h1>
+<footer class="py-5">
+  <section class=" d-flex justify-content-center m-auto align-items-center pt-5">
+    <div class="col-xl-11 m-auto">
+      <div class="container-fluid">
+        <div class="row d-flex justify-content-center">
+          <h2 class="h2-responsive ">DÊ O SEU PRIMEIRO <span class="px-2 bg-blue">PASSO</span></h2>
+        </div>
+        <div class="d-flex">
+          <a class=" m-auto d-flex white-text"><span class="px-5 bg-blue">VER MAIS</span></a>
+        </div>
+        <div class="d-flex flex-column justify-content-center align-items-center m-auto pt-5 pb-5">
+          <p class="white-text m-0 "> <span class="mx-2">FACEBOOK </span>| <span class="mx-2">INSTAGRAM </span>| <span class="mx-2">LINKEDIN</span></p>
+          <p class="white-text m-0 ">TODOS DIREITOS RESERVADOS © 2022 | CNPJ: 38.488.871/0001.79</p>
+
+
+        </div>
+      </div>
+    </div>
+  </section>
 </footer>
 
 <script src="<?php echo esc_url(get_template_directory_uri()); ?>/js/jquery.min.js"></script>
@@ -13,4 +30,5 @@
 <script src="https://todoprint.com.br/media/isidora/js/maskedinput/maskedinput.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js"></script>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <section class="d-flex justify-content-center m-auto align-items-center mt-5">
     <img loading="lazy" class="img-fluid position-img" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/imagens/logo-alpes.webp" alt="Logo Alpes">
   </section>
+
   <section class=" bg-alpes d-flex justify-content-center m-auto align-items-center">
     <div class="col-xl-11">
       <div class="container-fluid">
@@ -13,7 +14,7 @@
     </div>
   </section>
 
-  <section class="py-5 come-alpes">
+  <section class="pt-5 come-alpes">
     <div class="col-12 py-5">
       <div class="container-fluid">
         <div class="timeline-scroll d-none d-md-block">
@@ -30,7 +31,7 @@
               <h2 class="h2-responsive text-center">VENHA CAMINHAR COM OS GIGANTES</h2>
               <img loading="lazy" class="img-fluid d-flex m-auto" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/logo-alpes-small.svg" alt="Logo Alpes">
             </div>
-            <div class="row d-none d-lg-inline-flex">
+            <div class="row d-none d-lg-inline-flex my-5">
               <div class="col-xl-3 col-6 col-md-3 d-lg-flex b-right b-bottom p-1 py-1">
                 <div class="d-flex w-100 h-100 card-hover">
                   <img loading="lazy" class="img-fluid m-auto d-flex" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/joie.svg" alt="Logo Joie">
@@ -115,9 +116,191 @@
                 </div>
               </div>
             </div>
+            <div class="d-flex my-md-5">
+              <a class="btn-alpes m-auto"> CONHEÇA NOSSOS CLIENTES</a>
+            </div>
+            <div class="py-5">
+              <h2 class="h2-responsive text-center">CASES</h2>
+              <img loading="lazy" class="img-fluid d-flex m-auto" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/logo-alpes-small.svg" alt="Logo Alpes">
+            </div>
+            <img loading="lazy" class="img-fluid d-flex m-auto" src="https://cdn.discordapp.com/attachments/841400428134662155/1109530357012910082/Group_2.png" alt="Logo Alpes">
+          </div>
+          <div class="d-flex my-md-5">
+            <a class="btn-alpes m-auto">CASES DE SUCESSO</a>
+          </div>
+          <div class="py-5">
+            <div class="pt-5">
+              <h2 class="h2-responsive text-center">ANTES DE ESCALAR, PEÇA AJUDA AOS GIGANTES</h2>
+              <img loading="lazy" class="img-fluid d-flex m-auto" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/logo-alpes-small.svg" alt="Logo Alpes">
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-11 col-md-11 h-100 m-auto">
+          <div class="container-fluid">
+            <div class="row d-none d-flex my-5">
+              <div class="col-xl-3 col-6 col-md-3 d-lg-flex b-right p-1 py-1">
+                <div class="d-flex w-100 h-100 card-hover">
+                  <img loading="lazy" class="img-fluid m-auto d-flex" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/alpes.svg" alt="Logo Alpes">
+                </div>
+              </div>
+              <div class="col-xl-3 col-6 col-md-3 d-lg-flex b-right p-1 py-1">
+                <div class="d-flex w-100 h-100 card-hover">
+                  <img loading="lazy" class="img-fluid m-auto d-flex" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/fama.svg" alt="Logo Fama Media Marketing">
+                </div>
+              </div>
+              <div class="col-xl-3 col-6 col-md-3 d-lg-flex b-right p-1 py-1">
+                <div class="d-flex w-100 h-100 card-hover">
+                  <img loading="lazy" class="img-fluid m-auto d-flex" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/plim.svg" alt="Logo Plim">
+                </div>
+              </div>
+              <div class="col-xl-3 col-6 col-md-3 d-lg-flex p-1 py-1">
+                <div class="d-flex w-100 h-100 card-hover">
+                  <img loading="lazy" class="img-fluid m-auto d-flex" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/midiasos.svg" alt="Logo Midia SOS">
+                </div>
+              </div>
+
+              <div class="d-flex m-auto my-md-5">
+                <div class="py-5">
+                  <a class="btn-alpes m-auto">CASES DE SUCESSO</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="py-5">
+          <div class="pt-5">
+            <h2 class="h2-responsive text-center">ALPES NA MÍDIA</h2>
+            <img loading="lazy" class="img-fluid d-flex m-auto" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/logo-alpes-small.svg" alt="Logo Alpes">
           </div>
         </div>
       </div>
+      <div class="col-xl-11 col-md-11 h-100 m-auto">
+        <div class="container-fluid">
+          <div class="row d-none d-flex my-5">
+            <div class="col-xl-3 col-6 col-md-3 d-lg-flex b-right p-1 py-1">
+              <div class="d-flex w-100 h-100 card-hover">
+                <img loading="lazy" class="img-fluid m-auto d-flex" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/jp.svg" alt="Logo Alpes">
+              </div>
+            </div>
+            <div class="col-xl-3 col-6 col-md-3 d-lg-flex b-right p-1 py-1">
+              <div class="d-flex w-100 h-100 card-hover">
+                <img loading="lazy" class="img-fluid m-auto d-flex" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/jp.svg" alt="Logo Fama Media Marketing">
+              </div>
+            </div>
+            <div class="col-xl-3 col-6 col-md-3 d-lg-flex b-right p-1 py-1">
+              <div class="d-flex w-100 h-100 card-hover">
+                <img loading="lazy" class="img-fluid m-auto d-flex" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/jp.svg" alt="Logo Plim">
+              </div>
+            </div>
+            <div class="col-xl-3 col-6 col-md-3 d-lg-flex p-1 py-1">
+              <div class="d-flex w-100 h-100 card-hover">
+                <img loading="lazy" class="img-fluid m-auto d-flex" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/jp.svg" alt="Logo Midia SOS">
+              </div>
+            </div>
+
+            <div class="d-flex m-auto my-md-5">
+              <div class="py-5">
+                <a class="btn-alpes m-auto">CASES DE SUCESSO</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section class="bg-mountain d-flex justify-content-center  m-auto">
+      <div class="col-10 mt-4">
+        <div class="container-fluid">
+          <h2 class="text-center h2-responsive">DECIDIMOS COMBINAR NOSSAS EXPERTISES PARA JUNTOS MOSTRARMOS O HORIZONTE</h2>
+          <img loading="lazy" class="img-fluid d-flex m-auto" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/logo-alpes-small.svg" alt="Logo Alpes">
+          <div class="position-text mb-4">
+            <h2 class="text-center h2-responsive">DEPENDE MUITO MAIS DE ESTRATÉGIA DO QUE DE BRAVURA</h2>
+            <img loading="lazy" class="img-fluid d-flex m-auto" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/logo-alpes-small.svg" alt="Logo Alpes">
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+  </section>
+
+  <section class="bravery-alpes d-flex justify-content-center  py-5">
+    <div class="col-xl-11">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col m-auto">
+            <div class="d-flex  py-3 flex-column justify-content-center align-items-center">
+              <div class="circle-hover rounded-circle overlay zoom">
+                <img loading="lazy" class="img-fluid overlay zoom" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/casa.svg" alt="Casa">
+              </div>
+              <div class="card-body">
+                <p>Lorem ipsum</p>
+              </div>
+            </div>
+          </div>
+          <div class="col m-auto">
+            <div class="d-flex  py-3 flex-column justify-content-center align-items-center">
+              <div class="circle-hover rounded-circle overlay zoom">
+                <img loading="lazy" class="img-fluid overlay zoom" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/mosquetao.svg" alt="Mosquetão">
+              </div>
+              <div class="card-body">
+                <p>Lorem ipsum</p>
+              </div>
+            </div>
+          </div>
+          <div class="col m-auto">
+            <div class="d-flex  py-3 flex-column justify-content-center align-items-center">
+              <div class="circle-hover rounded-circle overlay zoom">
+                <img loading="lazy" class="img-fluid overlay zoom" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/bota.svg" alt="Bota">
+              </div>
+              <div class="card-body">
+                <p>Lorem ipsum</p>
+              </div>
+            </div>
+          </div>
+          <div class="col m-auto">
+            <div class="d-flex  py-3 flex-column justify-content-center align-items-center">
+              <div class="circle-hover rounded-circle overlay zoom">
+                <img loading="lazy" class="img-fluid overlay zoom" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/mochila.svg" alt="Mochila">
+              </div>
+              <div class="card-body">
+                <p>Lorem ipsum</p>
+              </div>
+            </div>
+          </div>
+          <div class="col m-auto">
+            <div class="d-flex  py-3 flex-column justify-content-center align-items-center">
+              <div class="circle-hover rounded-circle overlay zoom">
+                <img loading="lazy" class="img-fluid overlay zoom" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/corda.svg" alt="Corda">
+              </div>
+              <div class="card-body">
+                <p>Lorem ipsum</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex my-md-5">
+          <a class="btn-alpes m-auto">CONHEÇA NOSSOS SERVIÇOS</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="bg-adventure d-flex justify-content-center  m-auto">
+    <div class="col-10 mt-4">
+      <div class="container-fluid mt-5">
+        <h2 class="text-center h2-responsive">E ESSE É O PRIMEIRO PASSO DA AVENTURA</h2>
+        <img loading="lazy" class="img-fluid d-flex m-auto" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/logo-alpes-small.svg" alt="Logo Alpes">
+        <div class="d-flex my-md-5">
+          <a class="btn-alpes m-auto">DÊ O SEU PRIMEIRO PASSO</a>
+        </div>
+        <div class="position-text mb-4">
+            <img loading="lazy" class="img-fluid d-flex m-auto" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/logo-alpes-small.svg" alt="Logo Alpes">
+          </div>
+      </div>
+    </div>
     </div>
   </section>
 </main>
