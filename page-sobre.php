b5e256484ebdbda3823f215860587b87993053b6
danielcrubens <dcr.tecnologi@gmail.com>
2023-05-23 10:30:37 -0300

    page-sobre responsividade :memo:

--- page-sobre.php
+++ page-sobre.php
@@ -17,38 +17,37 @@
     </div>
   </section>
 
-  <section class="white bg-cloud">
-  <div class="teste "></div>
-
-    <div class="col-xl-10 d-flex justify-content-center m-auto align-items-center">
+  <section class="white bg-cloud-overlay">
+  <div class="bg-cloud "></div>
+    <div class="col-xl-10 col-md-10 d-flex justify-content-center m-auto align-items-center">
       <div class="container-fluid  my-5">
         <div class="py-5">
           <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas feugiat libero dui, eu commodo augue efficitur sit amet. Proin fringilla ipsum vitae felis molestie dignissim. In a luctus tortor. Donec aliquam odio nisl, in tempor leo pulvinar vel. Donec orci leo, tincidunt sed turpis tincidunt, interdum sollicitudin erat. Nunc a ligula erat. Aliquam dolor arcu, posuere ac sapien sed, suscipit vehicula mi. Quisque eleifend diam sit amet enim dignissim eleifend.</p>
         </div>
         <div class="row">
-          <div class="col-md-4">
-            <img class="img-fluid" src="https://cdn.discordapp.com/attachments/969661217142620203/1110281062556368966/Group_9.png" alt="">
+          <div class="col-md-4 mb-5 mb-md-0 d-flex justify-content-center d-lg-flex justify-content-lg-start">
+            <img class="img-fluid" src="https://cdn.discordapp.com/attachments/841400428134662155/1110553895362637926/Group_9.png" alt="">
           </div>
-          <div class="col-md-4">
-            <img class="img-fluid d-flex m-auto" src="https://cdn.discordapp.com/attachments/969661217142620203/1110281062556368966/Group_9.png" alt="">
+          <div class="col-md-4 mb-5 mb-md-0 d-flex justify-content-center d-lg-flex justify-content-lg-center">
+            <img class="img-fluid d-flex m-auto" src="https://cdn.discordapp.com/attachments/841400428134662155/1110553895362637926/Group_9.png" alt="">
           </div>
-          <div class="col-md-4">
-            <img class="img-fluid d-flex ml-auto" src="https://cdn.discordapp.com/attachments/969661217142620203/1110281062556368966/Group_9.png" alt="">
+          <div class="col-md-4 mb-5 mb-md-0 d-flex justify-content-center d-lg-flex justify-content-lg-end">
+            <img class="img-fluid d-flex ml-md-auto" src="https://cdn.discordapp.com/attachments/841400428134662155/1110553895362637926/Group_9.png" alt="">
           </div>
-          <div class="w-100 mt-5"></div>
-          <div class="col-md-4">
-            <img class="img-fluid" src="https://cdn.discordapp.com/attachments/969661217142620203/1110281062556368966/Group_9.png" alt="">
+          <div class="w-100 mt-md-5"></div>
+          <div class="col-md-4 mb-5 mb-md-0 d-flex justify-content-center d-lg-flex justify-content-lg-start">
+            <img class="img-fluid" src="https://cdn.discordapp.com/attachments/841400428134662155/1110553895362637926/Group_9.png" alt="">
           </div>
-          <div class="col-md-4">
-            <img class="img-fluid d-flex m-auto" src="https://cdn.discordapp.com/attachments/969661217142620203/1110281062556368966/Group_9.png" alt="">
+          <div class="col-md-4 mb-5 mb-md-0 d-flex justify-content-center d-lg-flex justify-content-lg-center">
+            <img class="img-fluid d-flex m-auto" src="https://cdn.discordapp.com/attachments/841400428134662155/1110553895362637926/Group_9.png" alt="">
           </div>
-          <div class="col-md-4">
-            <img class="img-fluid d-flex ml-auto" src="https://cdn.discordapp.com/attachments/969661217142620203/1110281062556368966/Group_9.png" alt="">
+          <div class="col-md-4 mb-md-0 d-flex justify-content-center d-lg-flex justify-content-lg-end">
+            <img class="img-fluid d-flex ml-md-auto" src="https://cdn.discordapp.com/attachments/841400428134662155/1110553895362637926/Group_9.png" alt="">
           </div>
         </div>
       </div>
     </div>
-    <div class="d-flex   dcr">
+    <div class="d-flex">
       <a class="btn-alpes m-auto">CONHEÇA NOSSOS SERVIÇOS</a>
     </div>
     <div class="my-5">
