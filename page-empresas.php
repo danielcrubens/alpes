a2839795b462dc23f86f32b19849ae653e58099e
danielcrubens <dcr.tecnologi@gmail.com>
2023-06-04 16:18:54 -0300

    page-empresas refatorando :fire:

--- page-empresas.php
+++ page-empresas.php
@@ -17,55 +17,74 @@
     </div>
   </section>
 
-  <section class="white-dark bg-cloud-overlay">
+  <section class="white-dark bg-cloud-overlay custom-post-companies">
     <div class="bg-cloud "></div>
-    <div class="col-xl-12 py-5 bg-cloud-left">
+    <div class="col-xl-12 py-5">
       <div class="container-fluid p-md-0 my-md-5">
         <div class="py-md-5">
           <h2 class="text-center h2-responsive">ANTES DE ESCALAR PEÇA AJUDA AOS GIGANTES</h2>
         </div>
+
         <div class="row mt-5">
-          <div class="col-md-6 p-0 m-0 ">
-            <img loading="lazy" class="img-fluid d-none d-md-block" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/imagens/empresas-alpes.webp" alt="Logo Alpes">
-            <img loading="lazy" class="img-fluid d-block d-md-none" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/logo-plim-amarelo.svg" alt="Logo Plim">
-          </div>
-          <div class="col-md-6 ">
-            <img loading="lazy" class="img-fluid d-md-flex m-auto d-none d-md-block" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/logo-alpes-azul.svg" alt="Logo Alpes">
-            <div class="ml-md-5">
-              <h3 class="h3-responsive m-0 d-none d-md-block">Alpes Mídia</h3>
-              <h3 class="h3-responsive m-0 d-block d-md-none text-center">Alpes Mídia</h3>
-              <p class="d-none d-md-block">alpesmidia.com.br</p>
-              <p class="d-block d-md-none text-center">alpesmidia.com.br</p>
-              <p class="last mb-5 mb-md-3 ">terdum sollicitudin erat. Nunc a ligula erat. Aliquam dolor arcu, posuere ac sapien sed, suscipit vehicula mi. Quisque eleifend diam sit amet enim dignissim eleifend. terdum sollicitudin erat. Nunc a ligula erat. Aliquam dolor arcu, posuere ac sapien sed, suscipit vehicula mi.</p>
-              <a class="btn-alpes m-auto m-0 d-none  d-md-inline-block">VER MAIS</a>
-            </div>
-          </div>
-          <div class="w-100 mt-md-5"></div>
-          <hr class=" w-75 mt-5 m-auto d-md-none d-block">
-          <div class="d-md-flex flex-row-reverse justify-content-center align-items-center">
-            <div class="col-md-6 p-0 m-0 text-right">
-              <img loading="lazy" class="img-fluid d-none d-md-block" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/imagens/empresas-alpes.webp" alt="Logo Alpes">
-            </div>
-            <div class="col-md-6">
-              <img loading="lazy" class="img-fluid mx-md-5 d-none d-md-block" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/logo-alpes-azul.svg" alt="Logo Alpes">
-              <img loading="lazy" class="img-fluid d-block d-md-none mt-5 mb-md-0" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/logo-plim-amarelo.svg" alt="Logo Plim">
-              <div class="mx-md-5">
-              <h3 class="h3-responsive m-0 d-none d-md-block">Alpes Mídia</h3>
-              <h3 class="h3-responsive m-0 d-block d-md-none text-center">Alpes Mídia</h3>
-              <p class="d-none d-md-block">alpesmidia.com.br</p>
-              <p class="d-block d-md-none text-center">alpesmidia.com.br</p>
-                <p class="last mb-5 mb-md-3">terdum sollicitudin erat. Nunc a ligula erat. Aliquam dolor arcu, posuere ac sapien sed, suscipit vehicula mi. Quisque eleifend diam sit amet enim dignissim eleifend. terdum sollicitudin erat. Nunc a ligula erat. Aliquam dolor arcu, posuere ac sapien sed, suscipit vehicula mi.</p>
-                <a class="btn-alpes m-auto d-none d-md-inline-block">VER MAIS</a>
-                <a class="btn-alpes m-auto d-md-none d-block">DÊ O SEU PRIMEIRO PASSO</a>
+          <?php
+          $args = array(
+            'post_type' => 'empresas',
+            'posts_per_page' => -1,
+          );
 
+          $empresas_query = new WP_Query($args);
 
+          if ($empresas_query->have_posts()) :
+            $iteration = 0;
+            while ($empresas_query->have_posts()) :
+              $empresas_query->the_post();
+          ?>
+              <div class="row mt-5">
+                <?php
+                if ($iteration % 2 == 0) {
+                ?>
+                  <div class="col-md-6 d-inline-flex align-items-stretch">
+                    <?php the_post_thumbnail('post-thumbnails', array('class' => 'img-fluid')); ?>
+                  </div>
+                  <div class="col-md-6 d-inline-block align-items-stretch">
+                    <img loading="lazy" class="img-fluid mx-md-5 mb-3 d-none d-md-block" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/logo-alpes-azul.svg" alt="Logo Alpes">
+                    <div class="mx-md-5">
+                      <h5 class="h5-responsive"><?php the_title(); ?></h5>
+                      <p class="last mb-md-3 "><?php the_excerpt(); ?></p>
+                      <a class="btn-alpes m-auto m-0   d-md-inline-block" href="<?php the_permalink(); ?>">
+                        VER MAIS
+                      </a>
+                    </div>
+                  </div>
+                <?php
+                } else {
+                ?>
+                  <div class="col-md-6 order-md-2">
+                    <?php the_post_thumbnail('meu-thumb', array('class' => 'img-fluid')); ?>
+                  </div>
+                  <div class="col-md-6 order-md-1">
+                    <img loading="lazy" class="img-fluid mx-md-5 d-none d-md-block" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/logo-alpes-azul.svg" alt="Logo Alpes">
+                    <div class="mx-md-5">
+                      <h5 class="h5-responsive"><?php the_title(); ?></h5>
+                      <p class="last mb-md-3 "><?php the_excerpt(); ?></p>
+                      <a class="btn-alpes m-auto m-0 d-none  d-md-inline-block" href="<?php the_permalink(); ?>">
+                        VER MAIS
+                      </a>
+                    </div>
+                  </div>
+                <?php
+                }
+                ?>
               </div>
-            </div>
-          </div>
+          <?php
+              $iteration++;
+            endwhile;
+          endif;
+          ?>
         </div>
       </div>
     </div>
- 
+
     <div class="my-md-5 overlay-bottom">
       <div class="  d-flex m-auto py-5">
         <img loading="lazy" class="img-fluid d-flex m-auto w-img" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/svg/logo-alpes-small.svg" alt="Logo Alpes">
@@ -74,4 +93,8 @@
 
   </section>
 </section>
+
+
+
+
 <?php get_footer(); ?>
