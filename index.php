e2401740c22a964a54420dd80b363a83b28ec419
danielcrubens <dcr.tecnologi@gmail.com>
2023-06-04 19:02:25 -0300

    correção rota :bug:

--- index.php
+++ index.php
@@ -6,17 +6,19 @@ if (is_post_type_archive('empresas')) {
 	get_template_part('page-empresas');
 } elseif (is_post_type_archive('noticias')) {
 	get_template_part('page-noticias');
+} elseif (is_post_type_archive('cases')) {
+	get_template_part('page-cases');
 } else {
 	// Restante do código para exibir o conteúdo do arquivo index.php
 	if (have_posts()) {
-			while (have_posts()) {
-					the_post();
-					// Restante do código para exibir o conteúdo do post
-					// por exemplo: the_title(), the_content(), etc.
-			}
+		while (have_posts()) {
+			the_post();
+			// Restante do código para exibir o conteúdo do post
+			// por exemplo: the_title(), the_content(), etc.
+		}
 	} else {
-			// Mensagem para quando não houver posts
-			echo 'Nenhum post encontrado.';
+		// Mensagem para quando não houver posts
+		echo 'Nenhum post encontrado.';
 	}
 }
 
