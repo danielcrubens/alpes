3d7ae36158bd154deba629fd71448ecb5933a999
danielcrubens <dcr.tecnologi@gmail.com>
2023-06-06 09:49:14 -0300

    page-sobre dinâmico  :fire:

--- page-sobre.php
+++ page-sobre.php
@@ -22,27 +22,27 @@
     <div class="col-xl-10 col-md-10 d-flex justify-content-center m-auto align-items-center">
       <div class="container-fluid scroll-down my-5">
         <div class="py-5">
-          <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas feugiat libero dui, eu commodo augue efficitur sit amet. Proin fringilla ipsum vitae felis molestie dignissim. In a luctus tortor. Donec aliquam odio nisl, in tempor leo pulvinar vel. Donec orci leo, tincidunt sed turpis tincidunt, interdum sollicitudin erat. Nunc a ligula erat. Aliquam dolor arcu, posuere ac sapien sed, suscipit vehicula mi. Quisque eleifend diam sit amet enim dignissim eleifend.</p>
+          <p class="text-center"><?php the_field('texto-sobre') ?></p>
         </div>
         <div class="row">
           <div class="col-md-4 mb-5 mb-md-0 d-flex justify-content-center d-lg-flex justify-content-lg-start">
-            <img class="img-fluid" src="https://cdn.discordapp.com/attachments/841400428134662155/1110553895362637926/Group_9.png" alt="">
+          <img class="img-fluid" src="<?php the_field('imagem-sobre-1'); ?>" alt="Grupo Alpes">
           </div>
           <div class="col-md-4 mb-5 mb-md-0 d-flex justify-content-center d-lg-flex justify-content-lg-center">
-            <img class="img-fluid d-flex m-auto" src="https://cdn.discordapp.com/attachments/841400428134662155/1110553895362637926/Group_9.png" alt="">
+            <img class="img-fluid d-flex m-auto" src="<?php the_field('imagem-sobre-2'); ?>" alt="Grupo Alpes">
           </div>
           <div class="col-md-4 mb-5 mb-md-0 d-flex justify-content-center d-lg-flex justify-content-lg-end">
-            <img class="img-fluid d-flex ml-md-auto" src="https://cdn.discordapp.com/attachments/841400428134662155/1110553895362637926/Group_9.png" alt="">
+            <img class="img-fluid d-flex ml-md-auto" src="<?php the_field('imagem-sobre-3'); ?>" alt="Grupo Alpes">
           </div>
           <div class="w-100 mt-md-5"></div>
           <div class="col-md-4 mb-5 mb-md-0 d-flex justify-content-center d-lg-flex justify-content-lg-start">
-            <img class="img-fluid" src="https://cdn.discordapp.com/attachments/841400428134662155/1110553895362637926/Group_9.png" alt="">
+            <img class="img-fluid" src="<?php the_field('imagem-sobre-4'); ?>" alt="Grupo Alpes">
           </div>
           <div class="col-md-4 mb-5 mb-md-0 d-flex justify-content-center d-lg-flex justify-content-lg-center">
-            <img class="img-fluid d-flex m-auto" src="https://cdn.discordapp.com/attachments/841400428134662155/1110553895362637926/Group_9.png" alt="">
+            <img class="img-fluid d-flex m-auto" src="<?php the_field('imagem-sobre-5'); ?>" alt="Grupo Alpes">
           </div>
           <div class="col-md-4 mb-md-0 d-flex justify-content-center d-lg-flex justify-content-lg-end">
-            <img class="img-fluid d-flex ml-md-auto" src="https://cdn.discordapp.com/attachments/841400428134662155/1110553895362637926/Group_9.png" alt="">
+            <img class="img-fluid d-flex ml-md-auto" src="<?php the_field('imagem-sobre-6'); ?>" alt="Grupo Alpes">
           </div>
         </div>
       </div>
